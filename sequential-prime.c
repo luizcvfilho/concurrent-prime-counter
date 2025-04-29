a25ce774764c4549fd13f7619b2972e9f89e150b
Luiz Claudio <lcvf10@gmail.com>
2025-04-29 20:41:40 -0300

    Sequential prime count

---
+++ sequential-prime.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <pthread.h>
+#include <time.h>
+
+int ehPrimo(long long int n) {
+    int i;
+    if (n <= 1) return 0;
+    if (n == 2) return 1;
+    if (n % 2 == 0) return 0;
+    for (i = 3; i < sqrt(n) + 1; i += 2)
+        if (n % i == 0) return 0;
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+    long long int N;
+    long long int prime_count = 0;
+    double start;
+    double end;
+    int c;
+    if (argc != 2) {
+        fprintf(stderr, "Digite: %s <valor de N> \n", argv[0]);
+        return 1;
+    }
+    N = atoll(argv[1]);
+
+    start = clock();
+
+    for (c = 0; c <= N; c++){
+        if (ehPrimo(c)){
+            prime_count++;
+        }
+    }
+
+    end = clock();
+
+    printf("N = %lld", N);
+    printf("Quantidade de primos encontrados = %lld\n", prime_count);
+    printf("Tempo: %.10f s\n", (end - start) / CLOCKS_PER_SEC);
+
+    return 0;
+}
